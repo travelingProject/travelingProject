64411318ee6d2ecfc0017ae6526c94fbe5ef2a46
Administrator <Administrator@User -2023UCZMH>
2023-11-03 18:16:46 +0900

    숙소 전체보기 작업중(이준섭)

--- traveling_project/WebContent/all_stays.jsp
+++ traveling_project/WebContent/all_stays.jsp
@@ -31,16 +31,59 @@
       	</div>
       	<div id="fee">
       		<h3>1박당 요금</h3>
-      		<div>
-      			<input type="range" id="input_left" min="1" max="101" value="1" step="10"/>
-      			<input type="range" id="input_right" min="1" max="101" value="101" step="10"/>
-      			<div class="track">
-      				<div class="range"></div>      			
-      				<div id="left_thumb" class="thumb"></div>      			
-      				<div id="right_thumb" class="thumb"></div>
-      			</div>      			
-      		</div>
-      	</div>      	
+		      <div class="price-input">		      	
+		        <div class="field">
+		          <span>이상</span>		          
+		          <span>이하</span>		          
+		        </div>		        
+		        <div class="field">		          
+		          <input type="text" class="input-min" value="0" />
+		          <span class="won">₩</span>
+		          <div class="separator">-</div>
+		          <input type="text" class="input-max" value="500,000" />
+		          <span class="won">₩</span>
+		        </div>
+		      </div>
+		      <div class="slider">
+		        <div class="progress"></div>
+		      </div>
+		      <div class="range-input">
+		        <input type="range" class="range-min" min="0" max="500000" value="0" step="10000" />
+		        <input type="range" class="range-max" min="0" max="500000" value="500000" step="10000" />
+		      </div>		      
+	      </div>
+	      <div id="filter">	      	
+	      	<h3>평점</h3>
+	      	<input id="rating5" type="checkbox" name="rating"> <label for="rating5">★ 4.5 ~ ★ 5.0</label> <br>
+	      	<input id="rating4.5" type="checkbox" name="rating"> <label for="rating4.5">★ 4.0 ~ ★ 4.5</label> <br>
+	      	<input id="rating4" type="checkbox" name="rating"> <label for="rating4">★ 3.5 ~ ★ 4.0</label> <br>
+	      	<input id="rating3.5" type="checkbox" name="rating"> <label for="rating3.5">★ 3.0 ~ ★ 3.5</label> <br>
+	      	<input id="rating3" type="checkbox" name="rating"> <label for="rating3">★ 3.0 이하</label> <br>	      	
+	      	<hr>
+	      	<h3>무료 서비스</h3>
+	      	<input type="checkbox"> 무료 주차 <br>
+	      	<input type="checkbox"> 무료 인터넷 <br>
+	      	<input type="checkbox"> 무료 취소 <br>
+	      	<hr>
+	      	<h3>편의 시설</h3>
+	      	<input type="checkbox"> 욕조 <br>
+	      	<input type="checkbox"> 세탁기 <br>
+	      	<input type="checkbox"> 건조기 <br>
+	      	<input type="checkbox"> 에어컨 <br>
+	      	<input type="checkbox"> 선풍기 <br>
+	      	<input type="checkbox"> 난방 <br>
+	      	<input type="checkbox"> 수영장 <br>
+	      	<input type="checkbox"> 오락실 게임 <br>
+	      	<input type="checkbox"> 헬스장 <br>
+	      	<input type="checkbox"> 보드게임 <br>
+	      	<input type="checkbox"> 바베큐 <br>
+	      	<input type="checkbox"> 기본 조리도구 <br>
+	      	<hr>
+	      	<h3>서비스</h3>
+	      	<input type="checkbox"> 아침 식사 <br>
+	      	<input type="checkbox"> 청소 서비스 <br>
+	      	<input type="checkbox"> 짐 보관 <br>
+	      </div>
       </aside>
       <section id="accomodation_info" class="clearfix">
         <div class="accomodation">
--- traveling_project/WebContent/js/all_stays.js
+++ traveling_project/WebContent/js/all_stays.js
@@ -1,42 +1,66 @@
-$(document).ready(function() {
-	const inputLeft = $('#input_left');
-	const inputRight = $('#input_right');
-	
-	const thumbLeft = $('#left_thumb');
-	const thumbRight = $('#right_thumb');
-	
-	const range = $('.range');
-	
-	const setLeftValue = (e) => {
-        const _this = e.target;
-        const { value, min, max } = _this;
+$(document).ready(function(){
+    const $rangeInput = $('.range-input input'),
+    $priceInput = $('.price-input input'),
+    $range = $('.slider .progress');
+    let priceGap = 10000;
+    
+    // 숫자에 콤마를 추가하는 함수
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    
+    // 문자열에서 콤마를 제거하는 함수
+    function removeCommas(str) {
+        return parseInt(str.replace(/,/g, ''), 10);
+    }
+    
+    $priceInput.on('input', function(e) {        
+        let minPrice = parseInt($priceInput.first().val().replace(/,/g, '')),
+            maxPrice = parseInt($priceInput.last().val().replace(/,/g, ''));
 
-        if (+inputRight.val() - +value < 10) {
-          _this.value = +inputRight.value - 10;
+        if (maxPrice - minPrice >= priceGap && maxPrice <= $rangeInput.last().prop('max')) {
+            if ($(this).hasClass('input-min')) {
+                $rangeInput.first().val(minPrice);
+                $range.css('left', (minPrice / $rangeInput.first().prop('max')) * 100 + '%');
+                $(this).val(numberWithCommas(minPrice));
+            } else {
+                $rangeInput.last().val(maxPrice);
+                $range.css('right', 100 - (maxPrice / $rangeInput.last().prop('max')) * 100 + '%');
+                $(this).val(numberWithCommas(maxPrice));
+            }
+        } else {
+            $(this).val(numberWithCommas($(this).val().replace(/,/g, ''))); // ensure to reformat the current input if not valid
         }
+    });    
+    
+    $rangeInput.on('input', function() {        
+        let minVal = parseInt($rangeInput.first().val()),
+            maxVal = parseInt($rangeInput.last().val());
 
-        const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-
-        $(thumbLeft).css('left', `${percent}%`);
-        $(range).css('left', `${percent}%`);
-	};
-	
-    const setRightValue = (e) => {
-        const _this = e.target;
-        const { value, min, max } = _this;
-
-        if (+value - +inputLeft.val() < 10) {
-          _this.value = +inputLeft.val() + 10;
+        if (maxVal - minVal < priceGap) {
+            if ($(this).hasClass('range-min')) {
+                $rangeInput.first().val(maxVal - priceGap);
+            } else {
+                $rangeInput.last().val(minVal + priceGap);
+            }
+        } else {
+            $priceInput.first().val(numberWithCommas(minVal));
+            $priceInput.last().val(numberWithCommas(maxVal));
+            $range.css('left', (minVal / $rangeInput.first().prop('max')) * 100 + '%');
+            $range.css('right', 100 - (maxVal / $rangeInput.last().prop('max')) * 100 + '%');
         }
+    });
+    
+    $priceInput.on('blur', function() {
+        // 콤마를 제거하고 정수로 변환
+        let value1 = removeCommas($priceInput.first().val());  
+        let value2 = removeCommas($priceInput.last().val());
+        console.log("value1 : " + value1 + ", value2 : " + value2);
+    });    
+    
+    // input에 focus하면 전체 선택
+    $priceInput.on('focus', function() {
+        $(this).select();
+    });   
 
-        const percent = ((+_this.value - +min) / (+max - +min)) * 100;
-
-        $(thumbRight).css('right', `${100 - percent}%`);
-        $(range).css('right', `${100 - percent}%`);
-      };
-      
-      if (inputLeft && inputRight) {
-    	  $(inputLeft).on('input', setLeftValue);
-    	  $(inputRight).on('input', setRightValue);
-    	}
 });
